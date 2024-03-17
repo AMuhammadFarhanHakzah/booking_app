c656cb1f2c5272f1a5efd329788e6825218153a5
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2024-03-17 10:16:36 +0800

    masih disituji

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderSection from "../"
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeaderSection   />
+    </div>
+  );
+};
+
+export default HomePage;
